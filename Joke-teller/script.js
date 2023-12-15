3c91a318570e3f5b7b6e75d5fbb131aa27221b88
SparshRai07 <cakelllkami@gmail.com>
2023-12-15 22:57:21 +0545

    joke teller project

---
+++ Joke-teller/script.js
@@ -0,0 +1,18 @@
+
+// voiceRSS javaScript SDK
+const VoicesRSS={speech(e){this._validate(e),this._request(e)},_validate(e)(if(!e))
+
+function test () {
+  VoicesRSS.speech({
+    VoiceRSS.speech({
+      key: '<41948497fe6a412083be0cb158b0148b>',
+      src: 'Hello, world!',
+      hl: 'en-us',
+      v: 'Linda',
+      r: 0, 
+      c: 'mp3',
+      f: '44khz_16bit_stereo',
+      ssml: false
+  });
+  }
+test();
